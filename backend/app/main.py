b151752f7c67ca23b33c0871df28b339fa372f35
Atul-Kumar29 <atulk29405@gmail.com>
2026-09-19 11:03:15 +0530

    Initial

---
+++ backend/app/main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+
+app = FastAPI(
+    title="UPI Behavioural Fraud Detection API",
+    version="1.0.0"
+)
+
+
+@app.get("/")
+def root():
+    return {
+        "message": "UPI Fraud Detection API"
+    }
+
+
+@app.get("/health")
+def health():
+    return {
+        "status": "healthy"
+    }
